0fbf5e97dc5cd78deacf2104c53b62f2f766db78
Alexander Barnes <abarnes@mines.edu>
2017-10-03 11:30:58 -0600

    Added smile

--- src/Cat.java
+++ src/Cat.java
@@ -46,8 +46,11 @@ public class Cat {
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
+		g2.setColor(Color.white);
+		g2.drawString("(____)",x-5,y+30);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
+		g2.setColor(Color.black);
 		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
